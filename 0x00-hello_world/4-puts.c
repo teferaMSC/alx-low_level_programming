1f37ea74cf912df2a8db08fdd6821f9757839bd8
teferaMSC <teferait@gmail.com>
2022-12-08 23:53:00 -0800

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<studio.h>
 /**
- * main - prints"Programming is like building a multilingual puzzle"
- * return 0
+ * main - prints"Programming is like building a multilingual puzzle, followed by a newline.
+ *Return: Always 0.
  */
 int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
