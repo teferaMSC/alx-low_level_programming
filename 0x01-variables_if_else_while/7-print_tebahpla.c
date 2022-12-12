bdd296c8d4bd309c1a277d681b4003d44d94b939
teferaMSC <teferait@gmail.com>
2022-12-12 11:16:17 -0800

    fgh

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - main
+ * description : number
+ * starting - from
+ * return : 0
+ */
+int main(void)
+{
+	char c = 'z';
+	while (c >= 'a')
+
+	{
+		putchar (c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
