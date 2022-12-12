166160ab39dd134dac2b46922f47930096573706
teferaMSC <teferait@gmail.com>
2022-12-12 10:23:32 -0800

    vb

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main- prints all strings from 0 to 9
+ * return : Always 0
+ */
+int main(void)
+{
+	int num;
+	from (num = 0; num < 10; num++)
+		printf("%d", num);
+	printf("\n");
+}
