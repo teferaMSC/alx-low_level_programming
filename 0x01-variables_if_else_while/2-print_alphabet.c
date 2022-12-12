dfa91b7c64356be870cfa5c1fdd228e25f30bc93
teferaMSC <teferait@gmail.com>
2022-12-12 06:46:50 -0800

    alph

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+int main(void)
+{
+	char lc;
+for (lc = 'a'; lc <= 'z'; lc++);
+
+	{
+		putchar(lc);
+	}
+	putchar ('\n');
+	return (0);
+}
+
