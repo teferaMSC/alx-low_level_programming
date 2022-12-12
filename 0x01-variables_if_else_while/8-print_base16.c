1aa0806068864995754d23675d9f63dc1e931c8c
teferaMSC <teferait@gmail.com>
2022-12-12 11:38:29 -0800

    cvb

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main -main
+ * description : prints all singel number of base 10
+ * starting from 0 to 9
+ * Return : always
+ */
+int main(void)
+{
+		for (ch = '0'; ch <= '9'; c++)
+		{
+		putcha(ch);
+		}
+		for (ch = 'a'; ch <= 'f'; ch++)
+		{
+		puctchar(ch);
+		}
+		puchchar(ch);
+		return (0);
+		}
