57c16a59d712fb48b379999b838759188232ca0a
teferaMSC <teferait@gmail.com>
2022-12-12 07:03:49 -0800

    alplowercse

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+int main(void)
+{
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+r(ch);
+	putchar('\n');
+	return (0);
+i}
