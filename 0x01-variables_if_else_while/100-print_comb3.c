ebe32bcfb58ba0f1a29cd0acaf4168ec80a779b9
teferaMSC <teferait@gmail.com>
2022-12-12 17:49:15 -0800

    c+d

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - main block
+ * descripution: write a program that print all posible
+ * number must be separeted by ','
+ * two digit must be different
+ */
+int main(void)
+{
+	int c;
+	int d = 0;
+
+	while (d < 10)
+	{
+		c = 0;
+
+		while (c < 10)
+		{
+			if (d != c && d < c)
+			{			{
+				putchar ('0' + d);
+				putchar (' ');
+				if (c + d != 17)
+				{
+			}
+		}
+		c++;
+	}
+	d++;
+}
+putchar ('\n');
+return (0);
+}
