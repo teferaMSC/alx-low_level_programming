6f2a08c7b1e10d2f6beb1d2073215ff3fe0419b5
teferaMSC <teferait@gmail.com>
2022-12-13 23:45:56 -0800

    alphabet ato z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * main - to print alphabate 
- *
+ * main - to print alphabate
+ * Description: print alphabet for a to z
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 	char c = 'a';
+
 	while (c <= 'z')
 	{
 		_putchar(c);
