d63d7207e4be70cef450b8a4539df48ba897f415
teferaMSC <teferait@gmail.com>
2022-12-14 03:46:41 -0800

    fg

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * descripution: print_alphabet_x10(void)
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int = 0;
+	while (i <= 10)
+	{
+		c = '0';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++
+	}
