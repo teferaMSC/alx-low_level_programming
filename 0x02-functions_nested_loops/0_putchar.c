f4435c7026bac0eb5740eeb7a5b7e3a0848eea0a
teferaMSC <teferait@gmail.com>
2022-12-13 22:02:22 -0800

    vf

---
+++ 0x02-functions_nested_loops/0_putchar.c
@@ -0,0 +1,26 @@
+#include "main.h"                                                                                                                       
+
+/**                                                                                                                                     
+ *
+ *  * main  - entry points                                                                                                                 
+ *
+ *   *                                                                                                                                      
+ *
+ *    * return: always 0 (success)
+ */
+
+int main(void)                                                                                                                          
+
+{                                                                                                                                       
+
+	        _putchar(' ')
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h')
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
+		return (0);
+		}
