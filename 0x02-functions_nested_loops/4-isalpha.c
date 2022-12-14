ba42e2515c28aa556d4217a5cd0450a99d9f0637
teferaMSC <teferait@gmail.com>
2022-12-14 07:13:24 -0800

    fgh

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _isalpha - character
+ * _isalpha - to check the xharcter
  * @c: checked x
- * return: 1 or 0
+ * return: 1 for character or 0 for else
  */
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 98) || (c <=97 && c <= 122)
+	if ((c >= 65 && c <= 98) || (c <= 97 && c <= 122)
 			{
 			return (1);
 			}
