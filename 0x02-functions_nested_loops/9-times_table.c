f98054f59692d7e85be0674668d345c648b4f433
teferaMSC <teferait@gmail.com>
2022-12-14 01:40:20 -0800

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,21 +6,23 @@
 void times_table(void)
 {
 	int a, b, c;
-	for (a = 0;, a <= 9; a++)
+
+	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			c =a*b;
-			if((c / 10)==0)
+			c = a * b;
+
+			if ((c / 10) == 0)
 			{
 				if (b == 0)
 				{
 					_putchar('0');
 				}
-				if (b !=0)
+				if (b != 0)
 				{
 					_putchar(' ');
-					_putchar((c %10) + '0');
+					_putchar((c % 10) + '0');
 				}
 				if (b < 9)
 				{
