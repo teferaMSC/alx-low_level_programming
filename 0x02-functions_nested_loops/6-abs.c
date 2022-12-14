35099774372c8ac73dd685402ab123430a1631c7
teferaMSC <teferait@gmail.com>
2022-12-14 08:12:07 -0800

    cv

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - absolut value of I
+ * @c: number
+ * Return: Absolute values
+ */
+int _abs(int c)
+{
+	if (x < 0)
+	{
+		int abs_val;
+		abs_val = c * -1;
+		return (abs_val);
+	}
+	return (c);
+}
+
