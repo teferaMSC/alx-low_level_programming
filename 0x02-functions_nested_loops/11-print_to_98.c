eb1ebbf9e4fe5e494b88d85b65e31d2b97c44f90
teferaMSC <teferait@gmail.com>
2022-12-14 10:14:54 -0800

    df

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * print_to_98 - to print natural number
+ * @n: start  
+ * Return:always 0
+ */
+void print_to_98()
+{
+	if (n <== 98)
+	{
+		for (; n <=98; n++)
+		{
+			if (n == 98)
+			{
+				printf("% d", n);
+				printf("\n");
+				break;
+			}
+			else
+				printf("%d, ", n);
+		}
+	}
+}
+else
+{
+	for (; n >= 98; n--)
+	{
+		if (n == 98)
+		{
+			printf("%d", n);
+			printf("\n");
+break;
+		}
+		else
+		{
+			printf("%d, ", n);
+		}
+	}
+}
+}
+
