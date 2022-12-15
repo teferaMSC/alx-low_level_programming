a214bfaba6827c362e2510a335b03c77524ddcb3
teferaMSC <teferait@gmail.com>
2022-12-15 00:58:41 -0800

    qw

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
 #include<time.h>
 #include<stdlib.h>
+/**
+ * main - print 4-print_alphabt.c
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	char la;
-	for (la = 'a'; la <= 'z'; la++
-			)
+
+	for (la = 'a'; la <= 'z'; la++)
 	{
 		if (la != 'e' && la != 'q')
 			putchar (la);
