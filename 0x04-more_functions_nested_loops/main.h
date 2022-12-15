1dc13d502fc3713fd78ffba7ff2913f32cb603ab
teferaMSC <teferait@gmail.com>
2022-12-15 07:23:09 -0800

    nm

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -17,9 +17,9 @@ int print_sign(int n);
 
 int _abs(int);                                                                                                                          
 
-int print_last_digit(int);                                                                                                              
+int print_las(int);                                                                                                              
 
-void jack_bauer(void);                                                                                                                  
+void (void);                                                                                                                  
 
 void times_table(void);                                                                                                                 
 
