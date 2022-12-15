93dcbfdaacc1842f2211d313d12d42cbfd7e8fcb
teferaMSC <teferait@gmail.com>
2022-12-15 07:53:52 -0800

    cv

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+include "main.h"
+/**
+ * print_most_numbers - number from 0-9 
+ *
+ * Return: number from 0-9 but not 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	 for (i = 0; i <= 9; i++)
+	 {
+		 if ((i == 2) || (i == 4))
+			 continue;
+		 else
+			 _putchar(i + '0');
+	 }
+	 _putchar('\n');
+}
