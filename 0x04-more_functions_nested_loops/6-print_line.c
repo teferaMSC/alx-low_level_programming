343150be93ace2b2244fad7da284369d7cc00938
teferaMSC <teferait@gmail.com>
2022-12-15 08:22:49 -0800

    cvb

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - the function that print line
+ * @n: input
+ * Return: line
+ */
+void print_line(line n)
+{
+int l;
+
+if (n <= 0)
+{
+	_putchar('\n');
+}
+else
+{
+	for (l = 1; l <= n; l++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
+}
