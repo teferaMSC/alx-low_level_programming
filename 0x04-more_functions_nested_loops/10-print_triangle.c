ed9e13910bcde207bb03ff86614b80ce197da919
teferaMSC <teferait@gmail.com>
2022-12-15 09:04:51 -0800

    bnm

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle - function that print triangle
+ * @s
+ * Return: the triangle size
+ */
+void print_triangle(int s)
+{
+	int a, r, t;
+
+	if (s <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (r = 0; r <= (s - 1); r++)
+		{
+			for (a = 0; a < (s - 1) -r; a++)
+			{
+				_putchar(' ');
+			}
+			for (t = 0; t <= r; t++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
