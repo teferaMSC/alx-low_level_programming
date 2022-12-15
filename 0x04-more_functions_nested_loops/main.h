d9ce856bc87097ca8694d6efb6ccc1a199cfe57d
teferaMSC <teferait@gmail.com>
2022-12-15 06:55:08 -0800

    cv

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,7 +9,7 @@ void print_numbers(void);
 
 void print_most_numbers(void);                                                                                                          
 
-int _islower(int c);                                                                                                                    
+int mul(int a, int b);                                                                                                                    
 
 int _isalpha(int c);                                                                                                                    
 
