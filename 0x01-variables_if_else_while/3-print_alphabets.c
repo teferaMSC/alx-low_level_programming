cd53c640f2d85fe560715f4ed336b9419c80c7db
teferaMSC <teferait@gmail.com>
2022-12-15 00:53:51 -0800

    hj

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
-#include<time.h>
-#include<stdlib.h>
+/**
+ * main - to print lower and upper case
+ *
+ * return: Always 0
+ */
 int main(void)
 {
+	char ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 	for (ch = 'A'; ch <= 'Z'; ch++)
-r(ch);
+		putchar(ch);
 	putchar('\n');
 	return (0);
-i}
+}
