100e8e35e9359cd6bbc34909f56c736ca724bb13
teferaMSC <teferait@gmail.com>
2022-12-18 21:05:55 -0800

    fg

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,35 +1,8 @@
-#ifndef HOLBERTON_H_INCLUDED                                                                                                            
-
-#define HOLBERTON_H_INCLUDED                                                                                                            
-
-int _putchar(char c);                                                                                                                   
-
-void print_alphabet(void);                                                                                                              
-
-void print_alphabet_x10(void);                                                                                                          
-
-int _islower(int c);                                                                                                                    
-
-int _isalpha(int c);                                                                                                                    
-
-int print_sign(int n);                                                                                                                  
-
-int _abs(int);                                                                                                                          
-
-int print_last_digit(int);                                                                                                              
-
-void jack_bauer(void);                                                                                                                  
-
-void times_table(void);                                                                                                                 
-
-int add(int, int);                                                                                                                      
-
-void print_to_98(int n);                                                                                                                
-
-void print_times_table(int n);                                                                                                          
-
-void num(int r, int n);                                                                                                                 
-
-                                                                                                                                        
-
-#endif
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+convert_day(int month, int day);
+print_remaining_days(int month, int day, int year);
+#endif /* MAIN_H */
