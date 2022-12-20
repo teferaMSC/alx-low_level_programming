de4313d98dd36f50ccf2f3bfe2c05393c2a52303
teferaMSC <teferait@gmail.com>
2022-12-19 21:24:35 -0800

    asdf

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,23 +1,13 @@
 #include "main.h"
 /**
  * _puts - prints  and  a new line
- * @str: to print string 
+ * @str: to print string
  */
-
 void _puts(char *str)
-
 {
-
-		while (*str != '\0')
-
-				{
-
-							_putchar(*str++);
-
-								}
-
-				_putchar('\n');
-
+while (*str != '\0')
+{
+	_putchar(*str++);
 }
-
-
+_putchar('\n');
+}
