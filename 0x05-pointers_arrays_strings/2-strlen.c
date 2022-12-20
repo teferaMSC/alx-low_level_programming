b76f9244ccbcba7c55e04b9aa9f87a7676c1c045
teferaMSC <teferait@gmail.com>
2022-12-19 21:14:58 -0800

    sd

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _puts - prints  and  a new line
+ * @str: to print string 
+ */
+
+void _puts(char *str)
+
+{
+
+		while (*str != '\0')
+
+				{
+
+							_putchar(*str++);
+
+								}
+
+				_putchar('\n');
+
+}
+
+
