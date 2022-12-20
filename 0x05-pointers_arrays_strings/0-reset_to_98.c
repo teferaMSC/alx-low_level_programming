9c0a864ebcf5fc7d355782e5ffef122f2909f972
teferaMSC <teferait@gmail.com>
2022-12-19 21:04:36 -0800

    sdfg

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
 /**
- *
- *  reset_to_98 - a function that make a pointe
- *@n: inpu 
-* Return: update
+ * reset_to_98 - a function that make a pointe
+ * @n: inpu
+ * Return: update
  */
 
 void reset_to_98(int *n)
