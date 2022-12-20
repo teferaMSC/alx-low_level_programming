39dac76f7bb240a18f69bfdd1505250d7e0b02c3
teferaMSC <teferait@gmail.com>
2022-12-20 04:49:42 -0800

    asd

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
- * _puts_half - a function that prints half of a string
- * if odd len, n = (length_of_the_string - 1) / 2
- * @str: input
- * Return: half of input
+ *puts_half - a function that prints half of a string
+ *if odd len, n = (length_of_the_string - 1) / 2
+ *@str: input
+ *Return: half of input
  */
-
 void puts_half(char *str)
 
 {
