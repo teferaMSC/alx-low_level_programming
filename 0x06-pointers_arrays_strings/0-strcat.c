e46526c155b94398f838e774dc935fd98252e2d0
teferaMSC <teferait@gmail.com>
2022-12-22 07:44:06 -0800

    vb

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,15 +1,17 @@
 #include"main.h"
 /**
- * strcat: string point
+ * stca: string point works for this task which is not posible short we hae ber
  * @dest: pointer
  * @src: sources
  * Return: destination
  */
-char *strcat(char *dest, const char *src)
+char *stca(char *dest, const char *src)
 {
 	int index = 0, dest_len = 0;
-	while (dest[index++]) dest_len++;
-	for (index = 0;src[index]; index++)
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
 	return (dest);
 }
