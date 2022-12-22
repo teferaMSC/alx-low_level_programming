dc82015a11688ea82b57568882070736e480de1b
teferaMSC <teferait@gmail.com>
2022-12-22 06:54:46 -0800

    asxd

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * file:- main.h
+ * T: tefera shimelis
+ * desc: header file
+ */
+int _putcharL(char c);
+char *_strcat(char *dest, char *src)
+	char*_strcat(char *dest, char *src, int n);
+	char*_strncpy(char *dest, char *src int n);
+	int _strcmp(char *s1, char *s2);
+	void reverse_array(int *a, int n);
