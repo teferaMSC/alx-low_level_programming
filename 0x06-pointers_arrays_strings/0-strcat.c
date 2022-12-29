a4aeacdd5c7600a5327e1f3e6be425f86c23b187
teferaMSC <teferait@gmail.com>
2022-12-28 23:41:24 -0800

    sde

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,17 +1,26 @@
 #include"main.h"
 /**
- * strcat: the string point to works for hts task which is not posible s
+ *_strcat: the string point to works for hts task which is not posible s
  * @dest: pointer
  * @src: sources
  * Return: destination
  */
-char *strcat(char *dest, const char *src)
+char *strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int i;
+	int j;
 
-	while (dest[index++])
-		dest_len++;
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
