92431c9ca9e96dc75920f91e6edfc50870c2c672
teferaMSC <teferait@gmail.com>
2022-12-29 00:11:50 -0800

    asd

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * _strcmp - comper two srting
+ * @s1: first string 
+ * @s2: second string
+ * Return: negatve diff.
+ *
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
