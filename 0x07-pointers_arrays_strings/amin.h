d6cd99bf3547c9399457f527de6633afabe66b2b
teferaMSC <teferait@gmail.com>
2023-01-02 06:01:54 -0800

    asd

---
+++ 0x07-pointers_arrays_strings/amin.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_h
+/**
+ * file: main.h
+ * Auth: wakanda it
+ * description: file name 
+ *
+ */
+#include <stddef.h>
+int _putchar(char c);
+void *_memset(void *s, int c, size_t n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strst
