1cfc77da9912517e09a80d9539ce9dd639ee3fe0
teferaMSC <teferait@gmail.com>
2023-01-02 06:27:08 -0800

    ase

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,6 +11,7 @@ void *_memset(void *s, int c, size_t n)
 {
 	unsigned int index;
 	unsigned char *memory = s, value = c;
+
 	for (index = 0; index < n; index++)
 		memory[index] = value;
 	return (memory);
