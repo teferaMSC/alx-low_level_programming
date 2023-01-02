bb61050a102963f2544be5bf991f7e7ee6e75773
teferaMSC <teferait@gmail.com>
2023-01-02 06:16:26 -0800

    asxdf

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,36 @@
+#ifndef MAIN_H                                                                                                                          
+
+#define MAIN_h                                                                                                                          
+
+/**                                                                                                                                     
+ *
+ *  * file: main.h                                                                                                                         
+ *
+ *   * Auth: wakanda it                                                                                                                     
+ *
+ *    * description: file name                                                                                                               
+ *
+ *     *                                                                                                                                      
+ *
+ *      */                                                                                                                                     
+
+#include <stddef.h>                                                                                                                     
+
+int _putchar(char c);                                                                                                                   
+
+void *_memset(void *s, int c, size_t n);                                                                                                
+
+char *_memcpy(char *dest, char *src, unsigned int n);                                                                                   
+
+char *_strchr(char *s, char c);                                                                                                         
+
+unsigned int _strspn(char *s, char *accept);                                                                                            
+
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+#endif
+
+
