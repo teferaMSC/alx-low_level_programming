7bbc28e50e6fcb45023c4945b5f99ab80e5f34d1
teferaMSC <teferait@gmail.com>
2023-01-02 07:08:00 -0800

    qw

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * print_chessbord - print
+ * @a: chessbord
+ */
+void print_chessbord(char (*a)[8])
+{
+	int index1, index2;
+
+	for (index1 = 0; a[index1][7]; index1++)
+	{
+		for (index2 = 0; index2 < 8; index2++)
+			_putchar(a[index1][index2]);
+		_putchar('\n');
+	}
+}
