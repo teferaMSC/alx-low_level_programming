ce4420143ff9741f712d7e47e75c516b89f2edea
teferaMSC <teferait@gmail.com>
2023-01-02 19:57:09 -0800

    asde

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,10 +7,12 @@ void print_chessbord(char (*a)[8])
 {
 	int index1, index2;
 
-	for (index1 = 0; a[index1][7]; index1++)
+	for (index1 = 0; index1 <= 7; index1++)
 	{
-		for (index2 = 0; index2 < 8; index2++)
+		for (index2 = 0; index2 <= 7; index2++)
+		{
 			_putchar(a[index1][index2]);
-		_putchar('\n');
+		}
+		_putchar(10);
 	}
 }
