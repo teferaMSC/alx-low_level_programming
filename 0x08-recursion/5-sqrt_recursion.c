7622046ec54586d605bf96cabdc838fe69b4d453
teferaMSC <teferait@gmail.com>
2023-01-04 07:21:49 -0800

    asw

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * _evaluate - fun sqrt
+ * _sqrt_recursion - fun sqrt
  * @i: int
  * @n: int
  * Return: sq
  */
-int _evaluate(int i, int n)
+int _sqrt_recursion(int i, int n)
 {
 	if (n == 0 || n == 1)
 		return (n);
 	else if (i * i < n)
-		return (_evaluate(i + 1, n));
+		return (_sqrt_recursion(i + 1, n));
 	else if (i * i == n)
 		return (i);
 	return (-1);
