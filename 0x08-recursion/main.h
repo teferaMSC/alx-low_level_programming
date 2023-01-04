5382102fcb299d3deb3d56efa1327c6958a99c40
teferaMSC <teferait@gmail.com>
2023-01-04 05:26:08 -0800

    asd

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,13 @@
+#ifdef MAIN_H
+#define MAIN_H
+/**
+ * file: main.h
+ * Auth: for my 
+ * desc: 0x08. C - Recursion
+ */
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s)
+
+#endif
