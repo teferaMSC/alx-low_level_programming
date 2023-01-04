46bde107ebabf50b6ce5af36430687dfc3051cd9
teferaMSC <teferait@gmail.com>
2023-01-04 06:02:14 -0800

    awe

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * _print_rev_recursion - revers
+ * @s: printed
+ */
+void _print_rev_recursion(s + 1)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
